e093aa513bc46474ff4e002a9ef8a6f2412f1e0d
RSeeth947 <rupesh.seeth@gmail.com>
2022-06-23 12:32:09 -0400

    added feature that tweets key events of game

--- getscores.py
+++ getscores.py
@@ -2,28 +2,31 @@ from bs4 import BeautifulSoup
 import requests
 
 url = 'https://www.espn.com/soccer/match/_/gameId/635042'
-mls_url = 'https://www.mlssoccer.com/competitions/u-s-open-cup/2022/matches/lavssac-06-21-2022/feed'
+comment_url = 'https://www.espn.com/soccer/commentary?gameId=635044'
 
 result = requests.get(url)
-mls_result = requests.get(mls_url).text
+comment_result = requests.get(comment_url).text
 
 doc = BeautifulSoup(result.text, "html.parser")
-mls_doc = BeautifulSoup(mls_result, "html.parser")
+comment_doc = BeautifulSoup(comment_result, "html.parser")
 
 
 class GameInfo:
 
     def __init__(self, url):
-        self.url = url
+        self.url = url[0]
         self.comment_url = url[1]
         self.result = requests.get(self.url)
-        self.comment_results = requests.get(self.comment_url)
+        self.comment_result = requests.get(self.comment_url)
         self.doc = BeautifulSoup(self.result.text, "html.parser")
-        self.comment_doc = BeautifulSoup(self.comment_results.text, "html.parser")
+        self.comment_doc = BeautifulSoup(self.comment_result.text, "html.parser")
 
     def refresh(self):
         self.result = requests.get(self.url)
         self.doc = BeautifulSoup(self.result.text, "html.parser")
+
+        self.comment_result = requests.get(self.comment_url)
+        self.comment.doc = BeautifulSoup(self.comment_result.text, "html.parser")
 
 
     def score_change(scores, current_score):
@@ -57,11 +60,22 @@ class GameInfo:
         awayscore = self.doc.find('span', class_="score icon-font-before").text.strip()
         return awayscore
 
-    def post_comments(self):
-        pass
+    def post_comments(self, comment_list):
+        container = self.comment_doc.find('div', id='match-commentary-1-tab-2')
+        comment_data = container.find_all('tr')
 
- 
-x = doc.find_all(class_="mls-o-match-feed__comment")
-print(x)
+        for item in comment_data:
+            comment = (item.find('td', class_='time-stamp').text,item.find('td', class_='game-details').string.strip())
+            if comment not in comment_list:
+                tweet = f"{comment[0]}: {comment[1]}"
+                comment_list.append(comment)
+                return tweet
+
+        return False
+
+
+
+
+
 
 
